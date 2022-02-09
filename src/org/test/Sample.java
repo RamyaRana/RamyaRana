f13af96305333eedef4193bae5278cec4313ada9
RamyaRana <ramyarajendran332@gmail.com>
2022-02-09 22:08:54 +0530

    New Coding Inserted

--- src/org/test/Sample.java
+++ src/org/test/Sample.java
@@ -1,6 +1,9 @@
 package org.test;
 
 public class Sample {
+	private void g1() {
+		System.out.println("New Coding Insert---sun");
+}
 public static void main(String[] args) {
 	System.out.println("hi");
 	System.out.println("git");
